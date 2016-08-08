78246027b6dd4acad3205eb68fe2c3d488441196
Aidi Stan <aidistan@live.com>
2016-08-08 21:42:33 +0800

    Refactor the codes

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,12 @@
   template(v-if="task")
     h3 参与者列表
     .avatars
-      avatar(v-for="id in involvedMembers", @click="addReciever(id)", :name="members[id].name", :url="members[id].avatarUrl", :has-liked="likedMembers[id] !== undefined")
+      avatar(v-for="id in involvedMembers", @click="addReciever(id)", :name="projectMember[id].name", :url="projectMember[id].avatarUrl", :has-liked="hasLikedMembers.includes(id)")
     h3 发私聊催促
     p 发送对象
-    span.reciever(v-for="id in message.recievers", @click="removeReciever(id)") {{ members[id].name }}
+    span.reciever(v-for="id in reminderMessage.recievers", @click="removeReciever(id)") {{ projectMember[id].name }}
     p 发送内容
-    textarea(rows="5", v-model="message.content")
+    textarea(rows="5", v-model="reminderMessage.content")
     .button(@click="sendMessage()") 发送
 </template>
 
@@ -23,17 +23,13 @@ export default {
       params: qs.parse(window.location.search.substr(1)),
       status: '加载中...',
       task: null,
-      members: {},
-      likedMembers: {},
-      message: {
+      projectMember: {},
+      involvedMembers: [],
+      hasLikedMembers: [],
+      reminderMessage: {
         recievers: [],
         content: ''
       }
-    }
-  },
-  computed: {
-    involvedMembers: function () {
-      return _.intersection(this.task.involveMembers, Object.keys(this.members))
     }
   },
   ready: function () {
@@ -51,29 +47,29 @@ export default {
     }).then((res) => {
       this.task = res.json()
 
-      this.message.content = `快来点赞任务「${this.task.content}」吧，就差你了：https://www.teambition.com/project/${this.task._projectId}/tasks/scrum/${this.task._tasklistId}/task/${this.task._id}`
-
       this.$http({
-        url: `https://api.teambition.com/api/tasks/${this.params.task}/like`,
+        url: `https://api.teambition.com/api/v2/projects/${this.task._projectId}/members`,
         method: 'GET',
         params: {
-          all: 1,
           access_token: this.access_token
         }
       }).then((res) => {
-        let likes = res.json().likesGroup
-        this.likedMembers = _.zipObject(_.map(likes, '_id'), likes)
+        let members = res.json()
+        this.projectMember = _.zipObject(_.map(members, '_id'), members)
+        this.involvedMembers = _.intersection(this.task.involveMembers, Object.keys(this.projectMember))
 
         this.$http({
-          url: `https://api.teambition.com/api/v2/projects/${this.task._projectId}/members`,
+          url: `https://api.teambition.com/api/tasks/${this.params.task}/like`,
           method: 'GET',
           params: {
+            all: 1,
             access_token: this.access_token
           }
         }).then((res) => {
-          let members = res.json()
-          this.members = _.zipObject(_.map(members, '_id'), members)
-          this.message.recievers = _.difference(this.involvedMembers, Object.keys(this.likedMembers))
+          let likes = res.json().likesGroup
+          this.hasLikedMembers = _.intersection(_.map(likes, '_id'), Object.keys(this.projectMember))
+          this.reminderMessage.recievers = _.difference(this.involvedMembers, this.hasLikedMembers)
+          this.reminderMessage.content = `快来点赞任务「${this.task.content}」吧，就差你啦：https://www.teambition.com/project/${this.task._projectId}/tasks/scrum/${this.task._tasklistId}/task/${this.task._id}`
 
           this.status = ''
         })
@@ -82,10 +78,10 @@ export default {
   },
   methods: {
     addReciever: function (id) {
-      this.message.recievers = _.union(this.message.recievers, [id])
+      this.reminderMessage.recievers = _.union(this.reminderMessage.recievers, [id])
     },
     removeReciever: function (id) {
-      this.message.recievers = _.difference(this.message.recievers, [id])
+      this.reminderMessage.recievers = _.difference(this.reminderMessage.recievers, [id])
     },
     sendMessage: function () {
       window.alert('抱歉，私聊提醒仍在开发中...')
